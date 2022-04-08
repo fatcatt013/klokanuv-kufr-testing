bc78b2f36f41431eea78bc99e6526a485673a046
Denis Kristak <denis.kristak@gmail.com>
2022-04-08 18:48:23 +0800

    adding admin url back

--- server/klokan/urls.py
+++ server/klokan/urls.py
@@ -16,15 +16,21 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from record_sheet import views
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
 router.register(r'subcategories', views.SubcategoryViewSet)
 router.register(r'categories', views.CategoryViewSet)
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
 ]
