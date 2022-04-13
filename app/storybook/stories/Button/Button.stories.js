1bab921e22ace6aab04bec5999232c6ba4faa425
richard <richard.klem@inuits.eu>
2022-04-13 14:20:59 +0200

    Add native storybook.

---
+++ app/storybook/stories/Button/Button.stories.js
@@ -0,0 +1,26 @@
+import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import { Text } from 'react-native';
+import Button from '.';
+import CenterView from '../CenterView';
+import { Button as Btn} from 'react-native-paper'
+
+storiesOf('Button', module)
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add('with text', () => (
+    <Button onPress={action('clicked-text')}>
+      <Text>{text('Button text', 'Hello Button')}</Text>
+    </Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ))
+    .add('Paper', () => (
+    <Btn mode={'outlined'}>
+        Paper Button
+    </Btn>
+));
