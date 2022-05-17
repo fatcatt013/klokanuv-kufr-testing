ab8ddf68a80eab2b7019361e9c80ff91651a12cb
richard <richard.klem@inuits.eu>
2022-05-17 13:57:55 +0200

    Add offline status indicator bar at the top of the app.

--- app/App.tsx
+++ app/App.tsx
@@ -5,20 +5,24 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { QueryClientProvider } from 'react-query'
-import { theme } from './src/theme';
-import { queryClient } from './src/utils';
-import { App } from './src';
+import { QueryClientProvider } from 'react-query';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { RecoilRoot } from 'recoil';
+import NetInfo from '@react-native-community/netinfo';
+import { queryClient } from './src/utils';
+import { theme } from './src/theme';
+import { App } from './src';
+import OfflineStatusBar from './src/components/OfflineStatusBar';
 
 const PERSISTENCE_KEY = 'NAVIGATION_STATE';
 
 export default () => {
   const [isReady, setIsReady] = React.useState(false);
   const [initialState, setInitialState] = React.useState();
+  const [offline, setOffline] = React.useState(false);
 
+  /* eslint-disable global-require */
   const [fontsLoaded] = useFonts({
     'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
     'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
@@ -26,6 +30,7 @@ export default () => {
     'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf'),
     'Luzanky-Display': require('./assets/fonts/Luzanky-Display.ttf'),
   });
+  /* eslint-enable global-require */
 
   React.useEffect(() => {
     const restoreState = async () => {
@@ -51,6 +56,12 @@ export default () => {
   if (!isReady || !fontsLoaded) {
     return <AppLoading />;
   }
+  // If you call the function returned from addEventListener(), you execute unsubscribe.
+  NetInfo.addEventListener((state) => {
+    if (!state.isConnected !== offline) {
+      setOffline(!state.isConnected);
+    }
+  });
 
   return <Provider theme={theme}>
     <RecoilRoot>
@@ -58,16 +69,15 @@ export default () => {
         <QueryClientProvider client={queryClient}>
           <NavigationContainer
             initialState={initialState}
-            onStateChange={state => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}
+            onStateChange={(state) => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}
           >
             <React.Suspense fallback={<AppLoading />}>
+              <OfflineStatusBar show={offline}/>
               <App />
             </React.Suspense>
           </NavigationContainer>
         </QueryClientProvider>
       </SafeAreaProvider>
     </RecoilRoot >
-  </Provider >
+  </Provider >;
 };
-
-//export {default} from './storybook';
