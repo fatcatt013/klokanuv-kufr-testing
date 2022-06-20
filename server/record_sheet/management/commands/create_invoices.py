d95974e9a0cc566d4d705c78e746021ec8dce58f
Petr Král <petr@inuits.eu>
2022-06-20 16:19:33 +0200

    edit invoice creation command

--- server/record_sheet/management/commands/create_invoices.py
+++ server/record_sheet/management/commands/create_invoices.py
@@ -5,14 +5,14 @@ from record_sheet import models
 class Command(BaseCommand):
     help = "Creates invoices for using the app for each school"
 
-    params = {param.name: param.value for param in models.Parameter.objects.all()}
-
     def handle(self, *args, **options):
-        for school in models.School.objects.exclude(name="SVČ Lužánky").filter(
-            is_subscriber=True
-        ):
+
+        params = {param.name: param.value for param in models.Parameter.objects.all()}
+
+        for school in models.School.objects.filter(is_subscriber=True):
 
             if school.is_prepaid:
                 school.is_prepaid = False
+                school.save()
             else:
-                models.Invoice.objects.create_invoice(school, self.params)
+                models.Invoice.objects.create_invoice(school, params)
